96ccd9d86fdc1e7af5a45f4d889b216c1e0597b5
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-06-25 12:16:51 +0300

    Add strong params to permit data from create events form to be read by create method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,5 +5,17 @@ class EventsController < ApplicationController
 
   def create
     @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :location, :date)
   end
 end
