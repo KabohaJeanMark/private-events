fb58d0164ab796ab5e90e58cf8e801ee7322f182
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-06-28 19:56:41 +0300

    Refactor using scope from Event model for my_past_events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user_events = current_user.events.all
     @my_upcoming_events = current_user.attended_events.upcoming
+    @my_past_events = current_user.attended_events.past
   end
 end
